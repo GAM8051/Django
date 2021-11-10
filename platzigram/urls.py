96312620272006247b32708e9327991d6b77f292
Julian Vega <juliandavid.vega@hotmail.com>
2021-11-10 17:22:13 -0500

    Manejo, Creacion de Usuarios sin modelo Django

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -5,6 +5,8 @@ from platzigram import views as local_views
 from posts import views as post_views
 
 urlpatterns = [
+
+    path("admin/", admin.site.urls),
     
     path("HelloWord/", local_views.hello_world),
     path("sorted/", local_views.sort_integers),
